47282b82ee2187134c1c964657a5c23459e4d097
cyberdot <cyberdot@users.noreply.github.com>
2026-09-20 16:15:07 +0100

    Updated projects to .net 10, upated nuget packages to their latest versionS (#1)

--- src/Jotter.Console/Program.fs
+++ src/Jotter.Console/Program.fs
@@ -2,42 +2,51 @@
 
 open System
 open System.CommandLine
-open System.CommandLine.Invocation
 
 
 [<EntryPoint>]
 let main argv =
-    
-    let root = RootCommand()
-    
-    let initCmd = Command("init", "Create the scaffolding for the static site")
-    initCmd.AddArgument(Argument<string>("theme", "Blog theme"))
-    initCmd.Handler <- CommandHandler.Create<string>(Action<string>(fun theme -> Jotter.Core.Site.init theme))
-    root.AddCommand(initCmd)
 
-    
+    let root = RootCommand("Jotter static site generator")
+
+    let initCmd = Command("init", "Create the scaffolding for the static site")
+    let themeArg = Argument<string>("theme")
+    themeArg.Description <- "Blog theme"
+
+    initCmd.Add(themeArg)
+    initCmd.SetAction(Action<ParseResult>(fun pr -> Jotter.Core.Site.init (pr.GetValue(themeArg))))
+    root.Add(initCmd)
+
+
     let buildCmd = Command("build", "Compile the site to the root weblog directory")
-    buildCmd.Handler <- CommandHandler.Create(Action(fun _ -> Jotter.Core.BuildTask.run()))
-    root.AddCommand(buildCmd)
-    
+    buildCmd.SetAction(Action<ParseResult>(fun _ -> Jotter.Core.BuildTask.run()))
+    root.Add(buildCmd)
+
     let pageCmd = Command("page", "Create new page")
-    pageCmd.AddArgument(new Argument<string>("title", "Page title"))
-    pageCmd.Handler <- CommandHandler.Create<string>(Action<string>(fun title -> Jotter.Core.PageTask.run title))
-    root.AddCommand(pageCmd)
-    
+    let pageTitleArg = Argument<string>("title")
+    pageTitleArg.Description <- "Page title"
+
+    pageCmd.Add(pageTitleArg)
+    pageCmd.SetAction(Action<ParseResult>(fun pr -> Jotter.Core.PageTask.run (pr.GetValue(pageTitleArg))))
+    root.Add(pageCmd)
+
     let postCmd = Command("post", "Create new post")
-    postCmd.AddArgument(new Argument<string>("title", "Post title"))
-    postCmd.Handler <- CommandHandler.Create<string>(Action<string>(fun title -> Jotter.Core.PostTask.run title))
-    root.AddCommand(postCmd)
-      
+    let postTitleArg = Argument<string>("title")
+    postTitleArg.Description <- "Post title"
+    postCmd.Add(postTitleArg)
+    postCmd.SetAction(Action<ParseResult>(fun pr -> Jotter.Core.PostTask.run (pr.GetValue(postTitleArg))))
+    root.Add(postCmd)
+
     let draftCmd = Command("draft", "Create new draft")
-    draftCmd.AddArgument(new Argument<string>("title", "Post title"))
-    draftCmd.Handler <- CommandHandler.Create<string>(Action<string>(fun title -> Jotter.Core.DraftTask.run title))
-    root.AddCommand(draftCmd)
-    
+    let draftTitleArg = Argument<string>("title")
+    draftTitleArg.Description <- "Post title"
+
+    draftCmd.Add(draftTitleArg)
+    draftCmd.SetAction(Action<ParseResult>(fun pr -> Jotter.Core.DraftTask.run (pr.GetValue(draftTitleArg))))
+    root.Add(draftCmd)
+
     let serverCmd = Command("server", "Start a local server to view the blog")
-    serverCmd.Handler <- CommandHandler.Create(Action(fun _ -> Jotter.Core.ServerTask.run()))
-    root.AddCommand(serverCmd)
-    
-    let result = root.Invoke(argv)
-    result
+    serverCmd.SetAction(Action<ParseResult>(fun _ -> Jotter.Core.ServerTask.run()))
+    root.Add(serverCmd)
+
+    root.Parse(argv).Invoke()
